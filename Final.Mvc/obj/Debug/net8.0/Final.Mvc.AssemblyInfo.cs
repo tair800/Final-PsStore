1e3b8a7ea33179da67c3ca3a3025d4ad82159695
tair800 <146845940+tair800@users.noreply.github.com>
2024-12-19 10:43:33 +0400

    creator name

--- Final.Mvc/obj/Debug/net8.0/Final.Mvc.AssemblyInfo.cs
+++ Final.Mvc/obj/Debug/net8.0/Final.Mvc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Final.Mvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a743e2c800c96bedee7a52925e862cf2f9e1a54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d1277a2c0818cab2cd7671b4a304df5a6e3733")]
 [assembly: System.Reflection.AssemblyProductAttribute("Final.Mvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Final.Mvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
